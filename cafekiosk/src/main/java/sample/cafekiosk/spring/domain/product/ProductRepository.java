bcae55567e43fe20efccaecf6ac79e1dad4dbbdd
lib26 <dladlsqja@naver.com>
2023-08-07 03:49:11 +0900

    feat: Persistence Layer 테스트

--- cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -11,6 +11,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
      * from product
      * where selling_status in ('SELLING', 'HOLD');
      */
+    // 이런 방식을 쿼리 메서드라고 한다. JPQL 작성 대신 메서드 네임으로 쿼리를 날림.
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
 
 }
